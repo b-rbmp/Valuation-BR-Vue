e6cc0e806e38c08412c473890f239e03ae21a66a
b-rbmp <b-rbmp@users.noreply.github.com>
2021-03-04 18:13:55 -0300

    Changed Vue 3 to Vue 2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import '@babel/polyfill';
 import 'mutationobserver-shim';
+import Vue from 'vue';
 import './plugins/bootstrap-vue';
-import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
