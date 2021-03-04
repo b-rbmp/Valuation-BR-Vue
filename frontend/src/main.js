fc594ff6b94f05c098e00638c0485b85657c318e
b-rbmp <b-rbmp@users.noreply.github.com>
2021-03-04 16:59:43 -0300

    Creation of the Front-End Project
    Creation of the Vue Boilerplate Project

---
+++ frontend/src/main.js
@@ -0,0 +1,8 @@
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import './plugins/bootstrap-vue';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+createApp(App).use(router).mount('#app');
