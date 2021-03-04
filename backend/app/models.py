f0ba2c5d6f014b72229c7b722f571042716dd08e
b-rbmp <b-rbmp@users.noreply.github.com>
2021-03-03 21:09:30 -0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -23,21 +23,6 @@ class Acao(db.Model):
     def __repr__(self):
         return '<ACAO {}>'.format(self.ticker)
         
-    def to_dict(self):
-        return dict(id=self.id,
-                    ticker=self.ticker,
-                    last_updated=self.last_updated.strftime('%Y-%m-%d %H:%M:%S'),
-                    dy=self.dy,
-                    patr_liq=self.patr_liq,
-                    roe=self.roe,
-                    vpa=self.vpa,
-                    lpa=self.lpa,
-                    n_acoes=self.n_acoes,
-                    lucro_liq=self.lucro_liq,
-                    payout=self.payout,
-                    margem_liq=self.margem_liq,
-                    cres5=self.cres5)
-
     @staticmethod
     def to_storage_format(float_num):
         return int(float_num*10000)
