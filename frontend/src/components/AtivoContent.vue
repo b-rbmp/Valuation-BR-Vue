6ca2f843a5e646d334e4d8e11c81c8f54d306461
b-rbmp <4665417+b-rbmp@users.noreply.github.com>
2021-03-08 12:43:15 -0300

    Data-showing front-end for Graham Method
    Created the data-showing front-end of the Graham valuation method, except for the charting part

--- frontend/src/components/AtivoContent.vue
+++ frontend/src/components/AtivoContent.vue
@@ -7,7 +7,48 @@
             Valuation de <b-badge pill variant="dark"> {{ $route.params.ativo }} </b-badge>
           </b-col>
         </b-row>
-        <br />
+        <br /><br />
+        <b-card no-body>
+          <b-tabs pills card content-class="mt-3" align="center" v-model="tabIndex">
+            <b-tab title="Graham" :title-link-class="linkClass(0)" active>
+              <b-container>
+                <b-row class="justify-content-md-center">
+                  <span class="display-4 method-title text-center">Método de Graham</span>
+                </b-row>
+                <b-row class="justify-content-md-center">
+                  <graham-content :preco_justo=1.00 :cotacao_atual=1.53 :upside=30.1 />
+                </b-row>
+              </b-container>
+            </b-tab>
+            <b-tab title="DCF(LPA)" :title-link-class="linkClass(1)">
+              <b-container>
+                <b-row class="justify-content-md-center">
+                  <h5 class="text-center">
+                    Método de Fluxo de Caixa Descontado com LPA (DCF-LPA)
+                  </h5>
+                </b-row>
+              </b-container>
+            </b-tab>
+            <b-tab title="Lynch" :title-link-class="linkClass(2)">
+              <b-container>
+                <b-row class="justify-content-md-center">
+                  <h5 class="text-center">
+                    Método de Lynch (Valor)
+                  </h5>
+                </b-row>
+              </b-container>
+            </b-tab>
+            <b-tab title="PSBe" :title-link-class="linkClass(3)">
+              <b-container>
+                <b-row class="justify-content-md-center">
+                  <h5 class="text-center">
+                    Método PSBe (Valor)
+                  </h5>
+                </b-row>
+              </b-container>
+            </b-tab>
+          </b-tabs>
+        </b-card>
       </b-container>
       <erro-content v-else />
     </div>
@@ -24,12 +65,14 @@
 
 <script>
 import ErroContent from './ativo_subcomponents/ErroContent.vue';
+import GrahamContent from './ativo_subcomponents/GrahamContent.vue';
 
 export default {
   name: 'AtivoContent',
   data() {
     return {
       ativos: null,
+      tabIndex: 0,
     };
   },
   methods: {
@@ -42,6 +85,12 @@ export default {
       }
       return false;
     },
+    linkClass(idx) {
+      if (this.tabIndex === idx) {
+        return ['bg-dark', 'text-white'];
+      }
+      return ['text-dark', 'nav-effect'];
+    },
   },
   beforeMount() {
     this.$store.dispatch('loadAtivos').then(() => {
@@ -50,8 +99,21 @@ export default {
   },
   components: {
     ErroContent,
+    GrahamContent,
   },
 };
 </script>
 
-<style scoped></style>
+<style>
+/* Not scoped so I can change Bootstrap background-color */
+.nav-effect:hover {
+  transition: 0.35s;
+  background-color: rgba(0, 0, 0, 0.2) !important;
+}
+</style>
+
+<style scoped>
+.method-title {
+  font-size: 2rem;
+}
+</style>
---
+++ frontend/src/components/ativo_subcomponents/GrahamContent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="graham-content mt-5">
+    <b-card-group deck>
+      <b-card
+        header-bg-variant="dark"
+        header="Preço Atual"
+        header-text-variant="white"
+        text-variant="dark"
+        class="card-indicator"
+        body-bg-variant="white"
+        align="center"
+        header-class="card-title"
+      >
+        <b-card-text class="lead">R$ {{ cotacao_atual.toFixed(2) }}</b-card-text>
+      </b-card>
+
+      <b-card
+        header-bg-variant="dark"
+        header="Preço Calculado"
+        header-text-variant="white"
+        text-variant="dark"
+        class="card-indicator"
+        body-bg-variant="white"
+        align="center"
+        header-class="card-title"
+      >
+        <b-card-text class="lead">R$ {{ preco_justo.toFixed(2) }}</b-card-text>
+      </b-card>
+
+      <b-card
+        header-bg-variant="dark"
+        header="Upside"
+        header-text-variant="white"
+        text-variant="dark"
+        class="card-indicator"
+        body-bg-variant="white"
+        align="center"
+        header-class="card-title"
+      >
+        <b-card-text :class="upsideClass()"> {{ upside.toFixed(2) }} %</b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GrahamContent',
+  props: {
+    preco_justo: Number,
+    cotacao_atual: Number,
+    upside: Number,
+  },
+  methods: {
+    upsideClass() {
+      if (this.upside > 0) {
+        return ['text-success', 'lead'];
+      }
+      if (this.upside < 0) {
+        return ['text-red', 'lead'];
+      }
+      return ['text-dark', 'lead'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-indicator {
+  min-width: 10rem;
+  max-width: 20rem;
+}
+.lead {
+  font-size: 1.5em;
+  font-weight: 300;
+}
+.card-title {
+  font-weight: 500;
+}
+</style>
