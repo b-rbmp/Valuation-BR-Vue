fced536b1ff3c1e569dab01cc185bf22064f0b73
b-rbmp <4665417+b-rbmp@users.noreply.github.com>
2021-03-30 22:16:16 -0300

    Deployment Update

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,14 +2,14 @@
 import axios from 'axios';
 
 export function fetchAtivos() {
-  return axios.get(`${process.env.VUE_APP_API_URL}/ativos`);
+  return axios.get(`${process.env.VUE_APP_API_URL}/ativos/`);
 }
 
 export function fetchAtivo(ativo) {
-  return axios.get(`${process.env.VUE_APP_API_URL}/ativo/${ativo}`);
+  return axios.get(`${process.env.VUE_APP_API_URL}/ativo/${ativo}/`);
 }
 
 export function fetchDCF(ativo) {
   // CRIAR LOGICA DO FORM DO DCF
-  return axios.post(`${process.env.VUE_APP_API_URL}/ativo/${ativo}`);
+  return axios.post(`${process.env.VUE_APP_API_URL}/ativo/${ativo}/`);
 }
